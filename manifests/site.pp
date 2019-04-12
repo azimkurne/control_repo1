c4a63db4b1ba9d0d9306b035c83d314915190102
Azim Kurne <34882603+azimkurne@users.noreply.github.com>
2019-04-12 12:41:22 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,6 @@ $filename = "/tmp/variable_test.txt"
 node 'master.puppet.vm' {
   file { $filename:
    	ensure => file,
-  	content => 'This puppet code is used test the variable declarations.\n',
+  	content => 'This puppet code is used test the variable declarations.',
   }
 }
