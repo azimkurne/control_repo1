2e246a5eed25084ce26f533b71b3871446727f03
Azim Kurne <34882603+azimkurne@users.noreply.github.com>
2019-04-12 14:57:04 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,7 @@
 node 'master.puppet.vm' {
   if $operatingsystem != 'CenteOS' {
     warning ("This operating system is not centOS.")
+    notify { "This operating system is ${::operatingsystem}.": }  
   }
   else {
     notify { "This operating system is ${::operatingsystem}, which is fine.": }  
